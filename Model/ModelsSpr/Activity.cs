7c0720ac4a83b6f263667833d4c627bef2065300
therolerofrome <105921270+therolerofrome@users.noreply.github.com>
2022-11-21 22:24:07 +0300

    ///

---
+++ Model/ModelsSpr/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ModelsSpr
+{
+    public class Activity
+    {
+        public int id { get; set; }
+        public string ActivityName { get; set; }    
+        public string objects { get; set; }
+        public int ZoomNumber { get; set; }
+        public string ActivityType { get; set; }
+        public string description { get; set; }
+    }
+}
